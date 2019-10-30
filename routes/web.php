509dba980317aa6c658278619519279460e6ee1f
jon <jpintool17dw@ikzubirimanteo.com>
2019-10-30 10:26:08 +0100

    4.1 finalizado

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('contacto');
+});
+
+Route::get('blog/id)',function($id){
+    return view('blog', array('id'=>$id));
+})->name('blog');
+
+Route::get('blog/{id}/{nombre}',function ($id, $nombre){
+return view('blog', ['id' =>$id, 'nombre'=>$nombre]);
+})->where(['id' => '[0-9]+', 'nombre' => '[A-Za-z]+'])->name('blog.nombreId');
+
